6d37e9c3ff17d20438a9a932bdc44a10e81a46f9
RennI <98748434+Renne103@users.noreply.github.com>
2024-03-03 20:56:19 +0300

    Add files via upload

---
+++ SourceFiles/CBubleSort.c
@@ -0,0 +1,310 @@
+﻿/*
+ * <кодировка символов>
+ *   Cyrillic (UTF-8 with signature) - Codepage 65001
+ * </кодировка символов>
+ *
+ * <сводка>
+ *   CBubleSort_B4ED74BE
+ * </сводка>
+ *
+ * <описание>
+ *   Данный исходный код описывает реализацию интерфейсов CBubleSort_B4ED74BE
+ * </описание>
+ *
+ * <автор>
+ *   Copyright (c) 2018 Vladimir Bashev. All rights reserved.
+ * </автор>
+ *
+ */
+
+#include "IEcoSystem1.h"
+#include "IEcoInterfaceBus1.h"
+#include "IEcoInterfaceBus1MemExt.h"
+#include "CBubleSort.h"
+
+/*
+ *
+ * <сводка>
+ *   Функция QueryInterface
+ * </сводка>
+ *
+ * <описание>
+ *   Функция QueryInterface для интерфейса IBubleSort
+ * </описание>
+ *
+ */
+int16_t ECOCALLMETHOD CBubleSort_B4ED74BE_QueryInterface(/* in */ struct IBubleSort* me, /* in */ const UGUID* riid, /* out */ void** ppv) {
+    CBubleSort_B4ED74BE* pCMe = (CBubleSort_B4ED74BE*)me;
+
+    /* Проверка указателей */
+    if (me == 0 || ppv == 0) {
+        return -1;
+    }
+
+    /* Проверка и получение запрошенного интерфейса */
+    if ( IsEqualUGUID(riid, &IID_IBubleSort) ) {
+        *ppv = &pCMe->m_pVTblIBubleSort;
+        pCMe->m_pVTblIBubleSort->AddRef((IBubleSort*)pCMe);
+    }
+    else if ( IsEqualUGUID(riid, &IID_IEcoUnknown) ) {
+        *ppv = &pCMe->m_pVTblIBubleSort;
+        pCMe->m_pVTblIBubleSort->AddRef((IBubleSort*)pCMe);
+    }
+    else {
+        *ppv = 0;
+        return -1;
+    }
+    return 0;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция AddRef
+ * </сводка>
+ *
+ * <описание>
+ *   Функция AddRef для интерфейса IBubleSort
+ * </описание>
+ *
+ */
+uint32_t ECOCALLMETHOD CBubleSort_B4ED74BE_AddRef(/* in */ struct IBubleSort* me) {
+    CBubleSort_B4ED74BE* pCMe = (CBubleSort_B4ED74BE*)me;
+
+    /* Проверка указателя */
+    if (me == 0 ) {
+        return -1;
+    }
+
+    return ++pCMe->m_cRef;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция Release
+ * </сводка>
+ *
+ * <описание>
+ *   Функция Release для интерфейса IBubleSort
+ * </описание>
+ *
+ */
+uint32_t ECOCALLMETHOD CBubleSort_B4ED74BE_Release(/* in */ struct IBubleSort* me) {
+    CBubleSort_B4ED74BE* pCMe = (CBubleSort_B4ED74BE*)me;
+
+    /* Проверка указателя */
+    if (me == 0 ) {
+        return -1;
+    }
+
+    /* Уменьшение счетчика ссылок на компонент */
+    --pCMe->m_cRef;
+
+    /* В случае обнуления счетчика, освобождение данных экземпляра */
+    if ( pCMe->m_cRef == 0 ) {
+        deleteCBubleSort_B4ED74BE((IBubleSort*)pCMe);
+        return 0;
+    }
+    return pCMe->m_cRef;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция MyFunction
+ * </сводка>
+ *
+ * <описание>
+ *   Функция
+ * </описание>
+ *
+ */
+void ECOCALLMETHOD CBubleSort_B4ED74BE_MyFunction(/* in */ struct IBubleSort* me, void * first, size_t size, size_t sizeType, int (ECOCDECLMETHOD * comparator ) (const void * , const void * )) {
+    CBubleSort_B4ED74BE* pCMe = (CBubleSort_B4ED74BE*)me;
+	size_t i = 0, j = 0, k = 0;
+	byte* arr = (byte*)first;
+	void * temp;
+	temp = pCMe->m_pIMem->pVTbl->Alloc(pCMe->m_pIMem, sizeType);
+	
+
+	for (i = 0; i < size - 1; ++i) {
+		for (j = 0; j < size - 1 - i; ++j){
+			if (comparator(arr + j*sizeType, arr + (j + 1)*sizeType)){
+				
+				pCMe->m_pIMem->pVTbl->Copy(pCMe->m_pIMem, temp, (arr + j*sizeType), sizeType);
+				pCMe->m_pIMem->pVTbl->Copy(pCMe->m_pIMem, (arr + j*sizeType), (arr + (j + 1)*sizeType), sizeType);
+				pCMe->m_pIMem->pVTbl->Copy(pCMe->m_pIMem, (arr + (j + 1)*sizeType), temp, sizeType);
+
+			}
+		}
+	}
+
+	pCMe->m_pIMem->pVTbl->Free(pCMe->m_pIMem, temp);
+}
+
+
+
+
+/*
+ *
+ * <сводка>
+ *   Функция Init
+ * </сводка>
+ *
+ * <описание>
+ *   Функция инициализации экземпляра
+ * </описание>
+ *
+ */
+int16_t ECOCALLMETHOD initCBubleSort_B4ED74BE(/*in*/ struct IBubleSort* me, /* in */ struct IEcoUnknown *pIUnkSystem) {
+    CBubleSort_B4ED74BE* pCMe = (CBubleSort_B4ED74BE*)me;
+    IEcoInterfaceBus1* pIBus = 0;
+    int16_t result = -1;
+
+    /* Проверка указателей */
+    if (me == 0 ) {
+        return result;
+    }
+
+    /* Сохранение указателя на системный интерфейс */
+    pCMe->m_pISys = (IEcoSystem1*)pIUnkSystem;
+
+    /* Получение интерфейса для работы с интерфейсной шиной */
+    result = pCMe->m_pISys->pVTbl->QueryInterface(pCMe->m_pISys, &IID_IEcoInterfaceBus1, (void **)&pIBus);
+
+    /* Проверка указателей */
+    if (me == 0 ) {
+        return result;
+    }
+
+    /* Сохранение указателя на системный интерфейс */
+    pCMe->m_pISys = (IEcoSystem1*)pIUnkSystem;
+
+
+
+    /* Освобождение */
+    pIBus->pVTbl->Release(pIBus);
+	
+    return result;
+}
+
+/* Create Virtual Table IBubleSort */
+IBubleSortVTbl g_x87CE395400A14271A107EB2788B31103VTbl_B4ED74BE = {
+    CBubleSort_B4ED74BE_QueryInterface,
+    CBubleSort_B4ED74BE_AddRef,
+    CBubleSort_B4ED74BE_Release,
+    CBubleSort_B4ED74BE_MyFunction
+};
+
+
+
+
+/*
+ *
+ * <сводка>
+ *   Функция Create
+ * </сводка>
+ *
+ * <описание>
+ *   Функция создания экземпляра
+ * </описание>
+ *
+ */
+int16_t ECOCALLMETHOD createCBubleSort_B4ED74BE(/* in */ IEcoUnknown* pIUnkSystem, /* in */ IEcoUnknown* pIUnkOuter, /* out */ IBubleSort** ppIBubleSort) {
+    int16_t result = -1;
+    IEcoSystem1* pISys = 0;
+    IEcoInterfaceBus1* pIBus = 0;
+    IEcoInterfaceBus1MemExt* pIMemExt = 0;
+    IEcoMemoryAllocator1* pIMem = 0;
+    CBubleSort_B4ED74BE* pCMe = 0;
+    UGUID* rcid = (UGUID*)&CID_EcoMemoryManager1;
+	
+    /* Проверка указателей */
+    if (ppIBubleSort == 0 || pIUnkSystem == 0) {
+        return result;
+    }
+
+    /* Получение системного интерфейса приложения */
+    result = pIUnkSystem->pVTbl->QueryInterface(pIUnkSystem, &GID_IEcoSystem1, (void **)&pISys);
+
+    /* Проверка */
+    if (result != 0 && pISys == 0) {
+        return result;
+    }
+
+    /* Получение интерфейса для работы с интерфейсной шиной */
+    result = pISys->pVTbl->QueryInterface(pISys, &IID_IEcoInterfaceBus1, (void **)&pIBus);
+
+	/* Получение идентификатора компонента для работы с памятью */
+    result = pIBus->pVTbl->QueryInterface(pIBus, &IID_IEcoInterfaceBus1MemExt, (void**)&pIMemExt);
+    if (result == 0 && pIMemExt != 0) {
+        rcid = (UGUID*)pIMemExt->pVTbl->get_Manager(pIMemExt);
+        pIMemExt->pVTbl->Release(pIMemExt);
+    }
+
+    /* Получение интерфейса распределителя памяти */
+    pIBus->pVTbl->QueryComponent(pIBus, rcid, 0, &IID_IEcoMemoryAllocator1, (void**) &pIMem);
+
+    /* Проверка */
+    if (result != 0 && pIMem == 0) {
+        /* Освобождение системного интерфейса в случае ошибки */
+        pISys->pVTbl->Release(pISys);
+        return result;
+    }
+
+    /* Выделение памяти для данных экземпляра */
+    pCMe = (CBubleSort_B4ED74BE*)pIMem->pVTbl->Alloc(pIMem, sizeof(CBubleSort_B4ED74BE));
+
+    /* Сохранение указателя на системный интерфейс */
+    pCMe->m_pISys = pISys;
+
+    /* Сохранение указателя на интерфейс для работы с памятью */
+    pCMe->m_pIMem = pIMem;
+
+    /* Установка счетчика ссылок на компонент */
+    pCMe->m_cRef = 1;
+
+    /* Создание таблицы функций интерфейса IBubleSort */
+    pCMe->m_pVTblIBubleSort = &g_x87CE395400A14271A107EB2788B31103VTbl_B4ED74BE;
+
+    /* Инициализация данных */
+    pCMe->m_Name = 0;
+
+    /* Возврат указателя на интерфейс */
+    *ppIBubleSort = (IBubleSort*)pCMe;
+
+    /* Освобождение */
+    pIBus->pVTbl->Release(pIBus);
+
+    return 0;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция Delete
+ * </сводка>
+ *
+ * <описание>
+ *   Функция освобождения экземпляра
+ * </описание>
+ *
+ */
+void ECOCALLMETHOD deleteCBubleSort_B4ED74BE(/* in */ IBubleSort* pIBubleSort) {
+    CBubleSort_B4ED74BE* pCMe = (CBubleSort_B4ED74BE*)pIBubleSort;
+    IEcoMemoryAllocator1* pIMem = 0;
+
+    if (pIBubleSort != 0 ) {
+        pIMem = pCMe->m_pIMem;
+        /* Освобождение */
+        if ( pCMe->m_Name != 0 ) {
+            pIMem->pVTbl->Free(pIMem, pCMe->m_Name);
+        }
+        if ( pCMe->m_pISys != 0 ) {
+            pCMe->m_pISys->pVTbl->Release(pCMe->m_pISys);
+        }
+        pIMem->pVTbl->Free(pIMem, pCMe);
+        pIMem->pVTbl->Release(pIMem);
+    }
+}
---
+++ UnitTestFiles/SourceFiles/BubleSort.c
@@ -0,0 +1,288 @@
+﻿/*
+ * <кодировка символов>
+ *   Cyrillic (UTF-8 with signature) - Codepage 65001
+ * </кодировка символов>
+ *
+ * <сводка>
+ *   BubleSort
+ * </сводка>
+ *
+ * <описание>
+ *   Данный исходный файл является точкой входа
+ * </описание>
+ *
+ * <автор>
+ *   Copyright (c) 2018 Vladimir Bashev. All rights reserved.
+ * </автор>
+ *
+ */
+
+
+/* Eco OS */
+#include "IEcoSystem1.h"
+#include "IdEcoMemoryManager1.h"
+#include "IdEcoInterfaceBus1.h"
+#include "IdEcoFileSystemManagement1.h"
+#include "IdBubleSort.h"
+#include <time.h>
+#include <stdlib.h>
+
+
+int ECOCDECLMETHOD comparatori(const void * a, const void * b) { 
+	int16_t data1 = *(int16_t *)a; 
+	int16_t data2 = *(int16_t *)b;
+    if (data1 > data2)
+		return 1;
+	else
+		return 0;
+}
+
+int ECOCDECLMETHOD comparatord(const void * a, const void * b) { 
+    double_t data1 = *(double_t *)a; 
+	double_t data2 = *(double_t *)b;
+    if (data1 > data2)
+		return 1;
+	else
+		return 0;
+}
+
+int ECOCDECLMETHOD comparatorc(const void * a, const void * b) { 
+    char data1 = *(char *)a; 
+	char data2 = *(char *)b;
+    if (data1 > data2)
+		return 1;
+	else
+		return 0;
+}
+
+
+
+/*
+ *
+ * <сводка>
+ *   Функция EcoMain
+ * </сводка>
+ *
+ * <описание>
+ *   Функция EcoMain - точка входа
+ * </описание>
+ *
+ */
+int16_t EcoMain(IEcoUnknown* pIUnk) {
+    int16_t result = -1;
+    /* Указатель на системный интерфейс */
+    IEcoSystem1* pISys = 0;
+    /* Указатель на интерфейс работы с системной интерфейсной шиной */
+    IEcoInterfaceBus1* pIBus = 0;
+    /* Указатель на интерфейс работы с памятью */
+    IEcoMemoryAllocator1* pIMem = 0;
+    char_t* name = 0;
+    char_t* copyName = 0;
+    /* Указатель на тестируемый интерфейс */
+    IBubleSort* pIBubleSort = 0;
+
+	int16_t arr_int[30] = {99, -2, 76, 96, -68, 36, -26, -21, 34, 52, 59, 85, 6, 75, -70, -92, -77, 70, -65, -3, 53, 100, 94, 84, -53, -95, 89, -20, -31, -10};
+	double_t arr_double[30] = {23.3083,70.2118,62.8812,16.4437,83.6103,48.9525,90.3603,70.3762,32.91,82.8918,81.0752,27.7077,97.2646,66.1746,80.7059,96.3442,93.0083,53.4421,28.3258,5.9416,70.6301,94.3844,50.879,90.7564,68.1696,28.374,69.9686,7.4785,35.6852,50.1796};
+	char_t arr_char[30] = {'J','S','B','9','b','C','Z','c','Z','K','W','D','f','m','U','3','5','a','e','c','Q','q','Y','t','d','7','p','j','g','f'};
+	int16_t size = 30;
+	size_t i;
+
+	
+	//Переменные для трекинга времени
+	double_t time_spent_b = 0.0;
+	double_t time_spent_q = 0.0;
+
+	clock_t begin_b;
+	clock_t end_b;
+
+	clock_t begin_q;
+	clock_t end_q;
+
+	//Переменные для задания количества итераций
+	int iter_1000 = 1000;
+	int iter_10000 = 10000;
+	int iter_100000 = 100000;
+	int iter_1000000 = 1000000;
+
+
+
+    /* Проверка и создание системного интрефейса */
+    if (pISys == 0) {
+        result = pIUnk->pVTbl->QueryInterface(pIUnk, &GID_IEcoSystem1, (void **)&pISys);
+        if (result != 0 && pISys == 0) {
+        /* Освобождение системного интерфейса в случае ошибки */
+            goto Release;
+        }
+    }
+
+    /* Получение интерфейса для работы с интерфейсной шиной */
+    result = pISys->pVTbl->QueryInterface(pISys, &IID_IEcoInterfaceBus1, (void **)&pIBus);
+    if (result != 0 || pIBus == 0) {
+        /* Освобождение в случае ошибки */
+        goto Release;
+    }
+#ifdef ECO_LIB
+    /* Регистрация статического компонента для работы со списком */
+    result = pIBus->pVTbl->RegisterComponent(pIBus, &CID_BubleSort, (IEcoUnknown*)GetIEcoComponentFactoryPtr_680786EC2FB742F2A3C6C9D4B4ED74BE);
+    if (result != 0 ) {
+        /* Освобождение в случае ошибки */
+        goto Release;
+    }
+#endif
+    /* Получение интерфейса управления памятью */
+    result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoMemoryManager1, 0, &IID_IEcoMemoryAllocator1, (void**) &pIMem);
+
+    /* Проверка */
+    if (result != 0 || pIMem == 0) {
+        /* Освобождение системного интерфейса в случае ошибки */
+        goto Release;
+    }
+
+    /* Выделение блока памяти */
+    name = (char_t *)pIMem->pVTbl->Alloc(pIMem, 10);
+
+    /* Заполнение блока памяти */
+    pIMem->pVTbl->Fill(pIMem, name, 'a', 9);
+
+
+    /* Получение тестируемого интерфейса */
+    result = pIBus->pVTbl->QueryComponent(pIBus, &CID_BubleSort, 0, &IID_IBubleSort, (void**) &pIBubleSort);
+    if (result != 0 || pIBubleSort == 0) {
+        /* Освобождение интерфейсов в случае ошибки */
+        goto Release;
+    }
+
+	//==========================================================================
+	//Test for Integer data
+	
+	begin_b = clock();
+	for (i = 0; i < iter_1000000; ++i){
+		pIBubleSort->pVTbl->MyFunction(pIBubleSort, arr_int, size, sizeof(int16_t), comparatori);
+	}
+	end_b = clock();
+    
+
+	begin_q = clock();
+	for (i = 0; i < iter_1000000; ++i){
+		qsort(arr_int, size, sizeof(int16_t), comparatori);
+	}
+	end_q = clock();
+
+
+	time_spent_b += (double)(end_b - begin_b);
+	time_spent_q += (double)(end_q - begin_q);
+	
+
+	printf("Bubble Sort: %lf \n", time_spent_b/1000.0);
+	printf("Q Sort: %lf \n", time_spent_q/1000.0);
+
+	printf("integers:\n");
+	for ( i = 0; i < 30; ++i) {
+		printf("%d ", arr_int[i]);
+	}
+	printf("\n\n");
+
+	time_spent_b = 0.0;
+	time_spent_q = 0.0;
+
+
+
+	//==========================================================================
+	//Test for Double data
+
+	begin_b = clock();
+	for (i = 0; i < iter_1000000; ++i){
+		pIBubleSort->pVTbl->MyFunction(pIBubleSort, arr_double, size, sizeof(double_t), comparatord);
+	}
+	end_b = clock();
+
+	begin_q = clock();
+	for (i = 0; i < iter_1000000; ++i){
+		qsort(arr_double, size, sizeof(double_t), comparatord);
+	}
+	end_q = clock();
+
+
+	time_spent_b += (double)(end_b - begin_b);
+	time_spent_q += (double)(end_q - begin_q);
+	
+
+	printf("Bubble Sort: %lf \n", time_spent_b/1000.0);
+	printf("Q Sort: %lf \n", time_spent_q/1000.0);
+
+	printf("doubles:\n");
+	for ( i = 0; i < 30; ++i) {
+		printf("%lf ", arr_double[i]);
+	}
+	printf("\n\n");
+
+	time_spent_b = 0.0;
+	time_spent_q = 0.0;
+
+
+	//==========================================================================
+	//Test for Char data
+
+	begin_b = clock();
+	for (i = 0; i < iter_1000000; ++i){
+		pIBubleSort->pVTbl->MyFunction(pIBubleSort, arr_char, size, sizeof(char_t), comparatorc);
+	}
+	end_b = clock();
+
+	begin_q = clock();
+	for (i = 0; i < iter_1000000; ++i){
+		qsort(arr_char, size, sizeof(char_t), comparatorc);
+	}
+	end_q = clock();
+
+
+	time_spent_b += (double)(end_b - begin_b);
+	time_spent_q += (double)(end_q - begin_q);
+	
+
+	printf("Bubble Sort: %lf \n", time_spent_b/1000.0);
+	printf("Q Sort: %lf \n", time_spent_q/1000.0);
+
+	printf("chars:\n");
+	for ( i = 0; i < 30; ++i) {
+		printf("%c ", arr_char[i]);
+	}
+	printf("\n");
+
+	time_spent_b = 0.0;
+	time_spent_q = 0.0;
+
+
+	//==========================================================================
+
+
+
+    /* Освлбождение блока памяти */
+    pIMem->pVTbl->Free(pIMem, name);
+
+Release:
+
+    /* Освобождение интерфейса для работы с интерфейсной шиной */
+    if (pIBus != 0) {
+        pIBus->pVTbl->Release(pIBus);
+    }
+
+    /* Освобождение интерфейса работы с памятью */
+    if (pIMem != 0) {
+        pIMem->pVTbl->Release(pIMem);
+    }
+
+    /* Освобождение тестируемого интерфейса */
+    if (pIBubleSort != 0) {
+        pIBubleSort->pVTbl->Release(pIBubleSort);
+    }
+
+
+    /* Освобождение системного интерфейса */
+    if (pISys != 0) {
+        pISys->pVTbl->Release(pISys);
+    }
+
+    return result;
+}
+
